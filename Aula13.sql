a83b9179bf2ee76f685ddf05da6b057d34057dd5
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2018-07-07 18:03:58 -0300

    Criando variável do tipo TABLE

---
+++ Aula13.sql
@@ -0,0 +1,6 @@
+--=================================================
+-- VARIÁVEL TABLE
+--=================================================
+DECLARE @TABELA_PESSOA TABLE(NOME VARCHAR(50), IDADE INT)
+
+INSERT INTO @TABELA_PESSOA(NOME, IDADE) VALUES('Paulo Sampaio', 22)
